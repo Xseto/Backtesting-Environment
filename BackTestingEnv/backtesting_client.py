c67b3c43435a730174b1af2ccd0f6acd6ac462e9
Xseto <108841689+Xseto@users.noreply.github.com>
2024-03-17 21:49:41 -0400

    Update backtesting_client.py

--- BackTestingEnv/backtesting_client.py
+++ BackTestingEnv/backtesting_client.py
@@ -1,37 +1,95 @@
-import socket
-import sys
+import socketserver
+import time
 import pandas as pd
-from datetime import datetime
+import os
 import re
-from io import StringIO
+from datetime import datetime
+import algo_trading_strat_1
 
-HOST, PORT = "localhost", 9999
+# provide a directory of files that will be used simulate real time data
+# for now, file names must include ticker, type, date in that order
+# for now, files must be xlsx
+# use regex to specify format
+# specify interval between sent data in seconds in cycle
 
-def opt_contract_dec(contract_string,as_string):
-    # Used for parsing an options contract name
-    option_regex = r"^([A-z]{1,5})(\d{6})([CPcp])([\d.]+)"
-    matches = re.findall(option_regex, contract_string)
-    if as_string:
-        return {'ticker':matches[0][0], 'date':matches[0][1], 'type':matches[0][2], 'strike':matches[0][3]}
-    else:
-        return {'ticker':matches[0][0], 'date':datetime.strptime(matches[0][1], '%y%m%d'), 'type':matches[0][2], 'strike':float(matches[0][3])/1000}
+symbols = '{}()[].,:;+-*/&|<>=~$1234567890_'
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
 
-    while True:
-        try:
-            received = str(sock.recv(8192), "utf-8")
-            a = pd.read_json(StringIO(received))
-            
-            contracts = opt_contract_dec(a['Contract Name'][0],False)['date'].strftime('%m/%d/%Y')
-
-            # For debugging
-            '''print('received {} {} contracts'.format(contracts, opt_contract_dec(a['Contract Name'][0],False)['type']))
-            print(a)
-            print('\n')
-            print('*******************************************************************************')'''
-        except Exception as e:
-            print(e)
-            break
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    # Mimics an exchange streaming market data
     
+    def fname_parser(self, fn, regex, as_string, date_form):
+        # parses filenames
+        matches = re.findall(regex, fn)
+        matches = [m for m in matches[0] if m not in symbols]
+        if as_string:
+            return {'fn':fn, 'ticker':matches[0], 'date':matches[2], 'type':matches[1], 'file_ext':matches[3]}
+        else:
+            return {'fn':fn, 'ticker':matches[0], 'date':datetime.strptime(matches[2], date_form), 'type':matches[1], 'file_ext':matches[3]}
+
+    def handle(self):
+        config = self.request.recv(1024).strip()
+        config_dict = eval(config)
+        cycle = config_dict['cycle']
+        data_structure = config_dict['data_structure']
+        directory = config_dict['directory']
+        regex = config_dict['regex']
+        date_form = config_dict['date_form']
+
+        if data_structure == 'one file':
+            # data is in one file, delivered row by row
+            dir = os.listdir(directory)
+            dir = [file for file in dir if file[0] != '~']
+            fn = dir[0]
+            fn_path = directory + '/' + fn
+            data = pd.read_excel(fn_path)
+            for i in data.index:
+                row = data.loc[i].to_json()
+                df_json = bytes(row, 'utf-8')
+                self.request.sendall(df_json)
+                time.sleep(cycle)
+
+        elif data_structure == 'multi files linked':
+            # data is in multiple files in single directory
+            # delivered row by row
+            dir = os.listdir(directory)
+            dir = [file for file in dir if file[0] != '~']
+            dir_parsed = [self.fname_parser(fn, regex, False, date_form) for fn in dir]
+            dir_parsed.sort(key=lambda x: x['date'])
+
+            for file in dir_parsed:
+                fn = file['fn']
+                fn_path = directory + '/' + fn
+                data = pd.read_excel(fn_path)
+                for i in data.index:
+                    row = data.loc[i].to_json()
+                    df_json = bytes(row, 'utf-8')
+                    self.request.sendall(df_json)
+                    time.sleep(cycle)
+
+        elif data_structure == 'multi files separate':
+            # data is in multiple files
+            # full dataframes delivered sheet by sheet
+            dir = os.listdir(directory)
+            dir = [file for file in dir if file[0] != '~']
+            dir_parsed = [self.fname_parser(fn, regex, False, date_form) for fn in dir]
+            dir_parsed.sort(key=lambda x: x['date'])
+
+            for file in dir_parsed:
+                fn = file['fn']
+                fn_path = directory + '/' + fn
+                try:
+                    xls = pd.ExcelFile(fn_path)
+                    for sheet in xls.sheet_names:
+                        df = pd.read_excel(xls, sheet).drop(columns=['Unnamed: 0', 'Last Trade Date', 'Change', '% Change'])
+                        df_json = bytes(df.to_json(), 'utf-8')
+                        self.request.sendall(df_json)
+                        time.sleep(.01)
+                except:
+                    break
+                time.sleep(cycle)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999 #localhost or 127.0.0.1
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        server.serve_forever()
